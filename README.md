91c4b8ed097f2c1231d17c561ad45527b07864a9
b1tsh1ft3r <b1tsh1ft3r@users.noreply.github.com>
2023-07-18 21:00:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#IPShield
+
+This is a quick and dirty bash script that relies on ipset and iptables to create a blacklist of known malicious IPs and block them
+in iptables with a single rule to drop any matches in the black list at the top of the INPUT chain.
+
+# Instructions
+
+This script was quickly developed on ubuntu server 22.04 and is hard coded in many places to be in /opt/ipshield/.
+Place this script there and make it executable (chmod +x ipshield.sh) and then cron the script to run once ever hour.
+
+#Update blocklists every hour and block with iptables
+0 */1 * * * /bin/bash /opt/ipshield/ipshield.sh > /dev/null
+
+This will create a rolling log of the updates happening in /var/log/ipshield.log when the script is triggered and it 
+will add a rule to iptables at the top of the INPUT chain which will drop any traffic if it matches one of the ips or
+subnets in the black list. 
+
+Enjoy!
+
+# References
+
+* https://www.ipdeny.com
+* https://www.opendbl.net
---
+++ ipshield.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+readonly log_file="/var/log/ipshield.log"
+readonly blacklist="/opt/ipshield/black.list"
+
+readonly opendbl_url="https://opendbl.net/lists/"
+readonly opendbl_files=("etknown.list" "tor-exit.list" "bruteforce.list" "blocklistde-all.list" "talos.list" "dshield.list" "sslblock.list")
+
+readonly ipdeny_url="https://www.ipdeny.com/ipblocks/data/countries/"
+readonly ipdeny_files=("cn.zone" "ru.zone")
+
+#####################################################################################
+# Error handling
+#####################################################################################
+log_error() 
+{ 
+    local timestamp=$(date); 
+    echo "$timestamp: Error - $1" >> "$log_file" 
+}
+
+#####################################################################################
+# Download lists
+#####################################################################################
+download_lists() 
+{
+    if [ ! -d "/opt/ipshield/blacklists" ]; then
+        mkdir -p "/opt/ipshield/blacklists"
+    fi
+    
+    # OpenDBL Lists
+    for filename in "${opendbl_files[@]}"; do
+        url="${opendbl_url}${filename}"
+        if wget -q -N "$url" -P "/opt/ipshield/blacklists/"; then
+            echo "$(date): Downloaded $filename" >> "$log_file"
+        else
+            log_error "Failed to download $filename from $url"
+        fi
+    done
+
+    # IPDeny Country Lists
+    for filename in "${ipdeny_files[@]}"; do
+      url="${ipdeny_url}${filename}"
+    if wget -q -N "$url" -P "/opt/ipshield/blacklists/"; then
+            echo "$(date): Downloaded $filename" >> "$log_file"
+        else
+            log_error "Failed to download $filename from $url"
+        fi
+    done
+}
+
+#####################################################################################
+# Create blacklist
+#####################################################################################
+function create_blacklist()
+{
+    echo "Creating blacklist..." >> "$log_file"
+    cat /opt/ipshield/blacklists/*.list > /opt/ipshield/black.list
+    cat /opt/ipshield/blacklists/*.zone >> /opt/ipshield/black.list
+    sed -i '/^#/d' /opt/ipshield/black.list                # remove comments from blacklist file
+    sed -i 's/^/add blacklist /' /opt/ipshield/black.list  # append required data to start of each line
+}
+
+#####################################################################################
+# Load blacklist
+#####################################################################################
+function load_blacklist() 
+{
+    echo "Loading blacklist..." >> "$log_file"
+
+    if iptables -nvL | grep -q "match-set blacklist"; then
+        iptables -D INPUT -m set --match-set blacklist src -j DROP
+        sleep 2
+    fi
+
+    ipset destroy blacklist                                # remove any existing blacklist
+    ipset -N blacklist iphash                              # create a blank blacklist
+    sleep 2
+    ipset restore < /opt/ipshield/black.list               # load the black.list file
+
+    # Check if the rule exists in the INPUT chain of iptables
+    if ! iptables -C INPUT -m set --match-set blacklist src -j DROP &>/dev/null; then
+        iptables -I INPUT 1 -m set --match-set blacklist src -j DROP
+    fi
+
+    total=$(cat /opt/ipshield/black.list | wc -l)          # count number of entries
+    echo "Loaded $total IPs"                               # Print total number of IPs loaded   
+}
+
+#####################################################################################
+# Main
+#####################################################################################
+function main 
+{
+    timestamp=$(date)
+    echo "$timestamp: Updating blacklists..." >> "$log_file"
+    
+    download_lists         # Download blacklists
+    create_blacklist       # Combine all lists into one
+    load_blacklist         # Load blacklist file
+
+    timestamp=$(date)
+    echo "$timestamp: Blacklist update complete!" >> "$log_file"
+}
+
+    main "$@"
